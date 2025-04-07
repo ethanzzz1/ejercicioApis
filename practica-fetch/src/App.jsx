8b5849f1cd0a190903e794bb479f82faf1c04921
ethan <20230006@ricaldone.edu.sv>
2025-04-07 15:14:44 -0600

    ola

--- practica-fetch/src/App.jsx
+++ practica-fetch/src/App.jsx
@@ -1,19 +1,37 @@
-import { BrowserRouter as Router, Routes, Route } 
-from "react-router"; 
-import Home from './pages/Home' 
-import Nav from "./components/Nav/Nav"; 
-import DragonBall from './pages/DragonBall'; 
-function App() { 
-return ( 
-<> 
-<Router> 
-<Nav /> 
-<Routes> 
-<Route path="/" element={<Home />} /> 
-<Route path="/dragon" element={<DragonBall/>} /> 
-</Routes> 
-</Router> 
-</> 
-) 
-} 
-export default App
+import { useEffect, useState } from 'react';
+import './App.css';
+function App() {
+  const [characters, setCharacters] = useState([]);
+  const [error, setError] = useState('');
+  useEffect(() => {
+    const fetchCharacters = async () => {
+      try {
+        const response = await fetch('https://rickandmortyapi.com/api/character');
+        const data = await response.json();
+        setCharacters(data.results);
+      } catch (e) {
+        setError('Error al cargar los personajes');
+      }
+    };
+    fetchCharacters();
+  }, []);
+  return (
+<div className="container">
+<h1>Personajes de Rick and Morty</h1>
+      {error && <p>{error}</p>}
+<div className="results">
+        {characters.map((character) => (
+<div className="card" key={character.id}>
+<img src={character.image} alt={character.name} />
+<h3>{character.name}</h3>
+<p><strong>Estado:</strong> {character.status}</p>
+<p><strong>Especie:</strong> {character.species}</p>
+<p><strong>Origen:</strong> {character.origin.name}</p>
+</div>
+        ))}
+</div>
+</div>
+  );
+}
+
+export default  App;
